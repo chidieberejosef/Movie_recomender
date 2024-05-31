949e9c46dd35f37efc3ab8b707783a6da879dbd8
Chidiebere Paul-joseph <121490509+chidieberejosef@users.noreply.github.com>
2024-05-31 11:13:23 +0100

    Update setup.py
    corrected syntax error

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description = 'A package for movies recommendation',
     long_description = long_description,
     long_description_content_type= 'text/markdown',
-    packages = find_packages(where=SRC_REPO)
+    packages = find_packages(where=SRC_REPO),
     python_requires = '>=3.7',
     install_requires = LIST_OF_REQUIREMENTS,
 )
